ab632147edf6daf4dda4389c2e7d8374da71838b
aigulmustafina <aimustafina2016@gmail.com>
2023-04-13 18:54:46 +0600

    Added headless mode for browser

--- conftest.py
+++ conftest.py
@@ -1,12 +1,21 @@
+import os
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='function')
 def browser():
     print('\nstart browser...')
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    if 'CI' in os.environ:
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    else:
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield browser
     print('\nquit browser...')
     browser.quit()
